a8f1de798f568a16319af1dbbaf03122b83e4925
Conlin Durbin <conlindurbin@protonmail.com>
2017-09-07 14:58:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,18 @@ if test ! $(which git); then
   exit 0
 fi
 
-#Download the repo and cd to fonts folder (Thanks to w0ng!)
-git clone https://github.com/w0ng/googlefontdirectory w0ng
-cd w0ng/fonts
+echo "INSTALLING GOOGLE FONTS"
+echo "-----------------------"
+
+#Download the repo and cd
+git clone https://github.com/google/fonts google_fonts
+cd google_fonts
 
 #Magical find command to cp all fonts to /Library/Fonts
 find . -name "*.ttf" -exec cp {} /Library/Fonts/ \;
 
 #All done, let's clean up a bit
 cd ../..
-rm -rf w0ng
+rm -rf google_fonts
+
+echo "--- Done! ---"
